169a6c65223fe6319bc6456885416f8ad8228609
Sergio <sergiderakes@gmail.com>
2020-02-06 15:15:28 +0100

    making progress

---
+++ gui test/test.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import functions
+
+class table(QtWidgets.QTableWidget):
+
+    def dropEvent(cls, self):
+        if self.hasText():
+            print("1")
+        else:
+            print("2")
+        return super().dropEvent(self)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tableWidget = table(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(0, 0, 801, 551))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.tableWidget.sizePolicy().hasHeightForWidth())
+        self.tableWidget.setSizePolicy(sizePolicy)
+        self.tableWidget.setAcceptDrops(True)
+        self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.AnyKeyPressed|QtWidgets.QAbstractItemView.DoubleClicked|QtWidgets.QAbstractItemView.EditKeyPressed)
+        self.tableWidget.setProperty("showDropIndicator", False)
+        self.tableWidget.setDragEnabled(True)
+        self.tableWidget.setDragDropMode(QtWidgets.QAbstractItemView.DragDrop)
+        self.tableWidget.setDefaultDropAction(QtCore.Qt.MoveAction)
+        self.tableWidget.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        dicc = functions.lector()
+        list_dicc = functions.dicc_to_tuple_list(dicc)
+        row = 0
+        for tup in list_dicc:
+            col = 0
+            if self.tableWidget.rowCount() <= row + 1:
+                self.tableWidget.setRowCount(row + 2)
+            for item in tup:
+                if self.tableWidget.columnCount() <= col + 1:
+                    self.tableWidget.setColumnCount(col + 2)
+                col+=1
+            row+=1
+        row = 0
+        for tup in list_dicc:
+            col = 0
+            for item in tup:
+                cellinfo = QtWidgets.QTableWidgetItem(item)
+                self.tableWidget.setItem(row, col, cellinfo)
+                col+=1
+            row+=1
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
--- test.py
+++ test.py
@@ -1,88 +1,9 @@
-import functions
+# -*- coding: utf-8 -*-
 
-def test_add_function():
-    print("Añadir una extensión a una carpeta que no existe: ")
-    print("Salida esperada: La extensión .jpg añadida a imagenes correctamente")
-    print("Salida obtenida: ", end = "")
-    functions.add_extension(".jpg", "imagenes")
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
 
-def test_add_function2():
-    print("Añadir una extensión a una carpeta que existe: ")
-    print("Salida esperada: La extensión .png añadida a imagenes correctamente")
-    print("Salida obtenida: ", end = "")
-    functions.add_extension(".png", "imagenes")
 
-def test_add_function_error():
-    print("Añadir una extensión que ya existe a una carpeta que existe: ")
-    print("Salida esperada: La extensión .jpg ya existe en imagenes")
-    print("Salida obtenida: ", end = "")
-    functions.add_extension(".jpg", "imagenes")
-
-def test_add_function_error2():
-    print("Añadir una extensión que ya existe a una carpeta que no existe: ")
-    print("Salida esperada: La extensión .jpg ya existe en imagenes")
-    print("Salida obtenida: ", end = "")
-    functions.add_extension(".jpg", "imagens")
-
-def test_delete_function():
-    print("Borrar una extensión: ")
-    print("Salida esperada: La extensión .jpg borrada de imagenes correctamente")
-    print("Salida obtenida: ", end = "")
-    functions.delete_extension(".jpg")
-
-def test_delete_function2():
-    print("Borrar la última extensión de una carpeta (borra también la carpeta): ")
-    print("Salida esperada: La extensión .png borrada de imagenes correctamente")
-    print("Salida obtenida: ", end = "")
-    functions.delete_extension(".png")
-
-def test_delete_function_error():
-    print("Borrar una extensión: ")
-    print("Salida esperada: La extensión .jpg no existe")
-    print("Salida obtenida: ", end = "")
-    functions.delete_extension(".jpg")
-
-def test_move_extension():
-    print("Mover extension: ")
-    print("Salida esperada: La extensión .txt añadida a textos correctamente\nLa extensión .txt borrada de textos correctamente\nLa extensión .txt añadida a texts correctamente")
-    print("Salida obtenida: ", end = "")
-    functions.add_extension(".txt", "textos")
-    functions.move_extension(".txt", "texts")
-
-def test_move_extension_error():
-    print("Mover extension: ")
-    print("Salida esperada: La extensión .txt ya se encuentra en la carpeta texts\nLa extensión .txt borrada de texts correctamente")
-    print("Salida obtenida: ", end = "")
-    functions.move_extension(".txt", "texts")
-    functions.delete_extension(".txt")
-
-def test_move_extension_error2():
-    print("Mover extension: ")
-    print("Salida esperada: La extensión .txt no existe")
-    print("Salida obtenida: ", end = "")
-    functions.move_extension(".txt", "textos")
-
-# Hay que borrar los datos del csv antes de ejecutar las pruebas
-# You must delete all the data from the csv before executing the test
-
-print("### PRUEBAS ###")
-print("----------------TEST 1: ")
-test_add_function()
-print("----------------TEST 2: ")
-test_add_function2()
-print("----------------TEST 3: ")
-test_add_function_error()
-print("----------------TEST 4: ")
-test_add_function_error2()
-print("----------------TEST 5: ")
-test_delete_function()
-print("----------------TEST 6: ")
-test_delete_function2()
-print("----------------TEST 7: ")
-test_delete_function_error()
-print("----------------TEST 8: ")
-test_move_extension()
-print("----------------TEST 9: ")
-test_move_extension_error()
-print("----------------TEST 10: ")
-test_move_extension_error2()
