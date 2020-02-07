267fa279ec7079255512890ac9826cc701548585
Sergio <sergiderakes@gmail.com>
2020-02-07 16:17:25 +0100

    mhe

--- gui test/dragable.py
+++ gui test/dragable.py
@@ -8,20 +8,79 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import random
+
+def create_label(Form, name, pos, color):
+        _translate = QtCore.QCoreApplication.translate
+        color = str(color[0]) + "," + str(color[1])+ "," + str(color[2])
+        label = lbl(Form)
+        label.setGeometry(QtCore.QRect(pos[0], pos[1], 120, 60))
+        label.setCursor(QtGui.QCursor(QtCore.Qt.OpenHandCursor))
+        label.setObjectName(name)
+        palette = QtGui.QPalette()
+        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.WindowText, brush)
+        label.setPalette(palette)
+        label.setText(_translate("Form", "Text Label"))
+        label.setStyleSheet("background: rgb(57,57,67);" + 
+                                "padding-left: 1px;" + 
+                                "border-left-width: 3px;" + 
+                                "border-left-color: rgb(" + color + ");" + 
+                                "border-left-style: solid")
+        # self.label.setFrameShape(QtWidgets.QFrame.StyledPanel) # QtWidgets.QFrame.Panel
+        # # self.label.setFrameShadow(QtWidgets.QFrame.Raised)
+        # self.label.setLineWidth(1)
+        # self.label.setMidLineWidth(0)
+        return label
+
+def generate_label_list(Form, base_name, pos, amount):
+    color = [(55, 239, 186),(30, 185, 128),(0, 93, 87),(0, 125, 81)] # random.randint(0, 3)
+    l = []
+    x, y = (0,0)
+    for i in range(amount):
+        label = create_label(Form, base_name + str(i), (pos[0] + x * 122, pos[1] + y * 62), color[i%4]) # 122 is 120 (width) + 2 (sep)
+        l.append(label)
+        if x == 5:
+            y += 1
+            y %= 6
+        x += 1
+        x %= 6
+    return l 
+
+def generate_coords(pos, label, width, height, sep):
+    x, y = pos[0], pos[1]
+    w, h = label.width(), label.height()
+    lx = []
+    ly = []
+    t = sep
+    for x1 in range(x, width * (w + t), w + t):
+        lx.append(x1)
+    for y1 in range(y, height * (h + t), h + t):
+        ly.append(y1)
+    l = []
+    for y in ly:
+        for x in lx:
+            t = (x, y)
+            l.append(t)
+    return l
+
 
 class lbl(QtWidgets.QLabel):
 
     dist_x = 0
     dist_y = 0
     grabbing = False
+    points = ()
 
     def mousePressEvent(self, QMouseEvent):
         # self.setFrameShadow(QtWidgets.QFrame.Sunken)
-        self.setStyleSheet("background: rgb(40,40,40);" + 
-                                "padding-left: 1px;" + 
-                                "border-left-width: 1px;" + 
-                                "border-left-color: rgb(0, 255, 0);" + 
-                                "border-left-style: solid;")
+        self.raise_()
+        temp = self.styleSheet()
+        splits = temp.split(";")
+        splits[0] = "background: rgb(67,67,77)"
+        temp = ";".join(splits)
+        self.setStyleSheet(temp)
         mp = QMouseEvent.pos()
         self.dist_x = mp.x()
         self.dist_y = mp.y()
@@ -38,16 +97,27 @@ class lbl(QtWidgets.QLabel):
         return super().mouseMoveEvent(QMouseEvent)
     
     def mouseReleaseEvent(self, QMouseEvent):
-        # self.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.setStyleSheet("background: rgb(30,30,30);" + 
-                                "padding-left: 1px;" + 
-                                "border-left-width: 1px;" + 
-                                "border-left-color: rgb(0, 255, 0);" + 
-                                "border-left-style: solid;")
-        self.grabbing = False
-        self.setCursor(QtGui.QCursor(QtCore.Qt.OpenHandCursor))
+        if self.grabbing:
+            self.check_position()
+            # self.setFrameShadow(QtWidgets.QFrame.Raised)
+            temp = self.styleSheet()
+            splits = temp.split(";")
+            splits[0] = "background: rgb(57,57,67)"
+            temp = ";".join(splits)
+            self.setStyleSheet(temp)
+            self.grabbing = False
+            self.setCursor(QtGui.QCursor(QtCore.Qt.OpenHandCursor))
         return super().mouseReleaseEvent(QMouseEvent)
     
+    def check_position(self):
+        x, y = self.get_center()
+        for xt, yt in self.points:
+            if x <= xt + self.width() and x >= xt and y <= yt + self.height() and y >= yt:
+                self.move(xt, yt)
+
+    def set_coords(self, points):
+        self.points = points
+
     def get_center(self):
         p = self.pos()
         w = self.width()
@@ -61,25 +131,12 @@ class Ui_Form(object):
         Form.setObjectName("Form")
         Form.resize(863, 574)
         Form.setAcceptDrops(True)
-        Form.setStyleSheet("background:rgb(25,25,25)")
-        self.label = lbl(Form)
-        self.label.setGeometry(QtCore.QRect(200, 150, 200, 100))
-        self.label.setCursor(QtGui.QCursor(QtCore.Qt.OpenHandCursor))
-        self.label.setObjectName("label")
-        palette = QtGui.QPalette()
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.WindowText, brush)
-        self.label.setPalette(palette)
-        self.label.setStyleSheet("background: rgb(30,30,30);" + 
-                                "padding-left: 1px;" + 
-                                "border-left-width: 1px;" + 
-                                "border-left-color: rgb(0, 255, 0);" + 
-                                "border-left-style: solid;")
-        # self.label.setFrameShape(QtWidgets.QFrame.StyledPanel) # QtWidgets.QFrame.Panel
-        # # self.label.setFrameShadow(QtWidgets.QFrame.Raised)
-        # self.label.setLineWidth(1)
-        # self.label.setMidLineWidth(0)
+        Form.setStyleSheet("background:rgb(44,44,54)")
+        self.label_list = generate_label_list(Form, "label", (50,50), 16)
+        points = generate_coords((50, 50), self.label_list[0], 4, 4, 2)
+        for label in self.label_list:
+            label.set_coords(points)
+        
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -87,7 +144,6 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
-        self.label.setText(_translate("Form", "TextLabel"))
 
 
 if __name__ == "__main__":
